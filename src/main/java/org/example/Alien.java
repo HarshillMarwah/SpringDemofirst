497c3a56f54823ea7ffdbe42700541d21230368f
Harshil IJ <marwahharshil@gmail.com>
2024-12-26 13:38:07 +0530

    Testing object creation. Getting all beans names.

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Alien {
+    public Alien(){
+        System.out.println("Alien OBJ created");
+    }
     public void code()
     {
         System.out.println("In function code.");
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,7 +15,14 @@ public class App
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         //Alien obj = new Alien();
-        Alien obj = (Alien) context.getBean("alien");
+//        Alien obj = (Alien) context.getBean("alien",Alien.class);
+        String[] beanNames = context.getBeanDefinitionNames();
+
+        // Print all the bean names
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+        Alien obj = (Alien) context.getBean("org.example.Alien#0");
         obj.code();
 
     }
