75440d53220cd3bc00d8dfbf1f5734d70298f77b
Harshil IJ <marwahharshil@gmail.com>
2024-12-28 01:00:53 +0530

    Scope trying and using Setter Injection.

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -1,9 +1,21 @@
 package org.example;
 
 public class Alien {
+    private int age;
     public Alien(){
-        System.out.println("Alien OBJ created");
+       // age=0;
+        System.out.println("Alien OBJ created; age="+age);
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Inside setter");
+        this.age = age;
+    }
+
     public void code()
     {
         System.out.println("In function code.");
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,7 +15,7 @@ public class App
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         //Alien obj1 = new Alien();
-        String[] beanNames = context.getBeanDefinitionNames();
+ /*       String[] beanNames = context.getBeanDefinitionNames();
 
         // Print all the bean names
         for (String beanName : beanNames) {
@@ -25,6 +25,15 @@ public class App
         Alien obj1 = (Alien) context.getBean("org.example.Alien#0");// getting bean without ID.
         obj.code();
         obj1.code();
+        //Scope testing.
+        Alien obj2 = (Alien) context.getBean("alien2");
+        obj.age=100;
+        System.out.println("obj2 age="+obj.age);
+        obj.code();
+*/
+        //Testing Dependency Injection
+        Alien obj = (Alien) context.getBean("alien",Alien.class);
+        System.out.println(obj.getAge());
 
     }
 }
